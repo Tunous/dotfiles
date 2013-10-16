23f6373488bf182decc2f8e521c6f9d68708b661
Thanel <lukus178@gmail.com>
2013-10-16 15:29:15 +0200

    Update autolink program

--- test.bat
+++ test.bat
@@ -3,12 +3,14 @@ title Autolink
 
 goto init
 
-:: ------------------------------------------------------------------------
+rem Start program ----------------------------------------------------------
 :init
   set target=%~dp0
+  rem TODO: Delete me, I'm temprorary:
+    goto select_links
   goto check_Permissions
 
-:: ------------------------------------------------------------------------
+rem Check administrator permissions ----------------------------------------
 :check_Permissions
   echo Administrative permissions required. Detecting permissions...
 
@@ -21,73 +23,126 @@ goto init
     goto end
   )
 
+rem Select what to link ----------------------------------------------------
+:select_links
+  echo Select what you want to link:
+  echo 1. Vim
+  echo 2. Vimperator
+  echo 3. Both
+  echo.
+
+  set /p choose=
+  cls
+  if %choose% gtr 3 ( goto error )
+  if %choose% lss 1 ( goto error )
+
+  goto check_Path
+
+rem Check if current location contains repository -------------------------
 :check_Path
   cd %target%
 
-  if exist vim\vimrc (
-    if exist vimperator\vimperatorrc (
-      goto link_Files
+  if %choose% == 1 (
+    if exist vim/vimrc (
+      goto link_Vim
+    )
+  )
+  if %choose% == 2 (
+    if exist vimperator/vimperatorrc (
+      goto link_Vimp
+    )
+  )
+  if %choose% == 3 (
+    if exist vim\vimrc (
+      if exist vimperator\vimperatorrc (
+        goto link_Vim
+      )
     )
   )
 
-  if %errorLevel% == 1 ( goto end )
+  if %errorLevel% == 1 ( goto error )
 
-  echo.
   echo Looks like repository is not here.
   goto get_Target
 
-:: ------------------------------------------------------------------------
+rem Get from user location of repository -----------------------------------
 :get_Target
-  :: Get location of repository
-  set /p target="Please write correct path of repository: " %=%
+  set /p target="Please write correct path of repository: "
 
   if %errorLevel% == 0 (
     cls
     goto check_Path
   ) else (
-    goto end
+    goto error
   )
 
-:: ------------------------------------------------------------------------
-:link_Files
-  :: Go to home directory
+rem Link vim files ---------------------------------------------------------
+:link_Vim
+  rem Go to home directory
   cd %USERPROFILE%
 
-  echo.
-  echo Linking files from "%target%" to "%USERPROFILE%"
+  echo Linking vim files.
   echo.
 
-  :: Link folders
   if exist vimfiles (
     echo "vimfiles" folder already exists. Skipping to next file.
   ) else (
     mklink /D vimfiles %batchdir%vim
   )
+  if exist _vimrc (
+    echo "_vimrc" already exists. Skipping to next file.
+  ) else (
+    mklink _vimrc %batchdir%vim\vimrc
+  )
+
+  if %choose% == 3 (
+    echo.
+    set choose=0
+    goto link_Vimp
+  )
+
+  if %errorLevel% == 0 (
+    goto success
+  ) else (
+    goto error
+  )
+
+rem Link vimperator files --------------------------------------------------
+:link_Vimp
+  rem Go to home directory
+  cd %USERPROFILE%
+
+  echo Linking vimperator files.
+  echo.
+
   if exist vimperator (
     echo "vimperator" folder already exists. Skipping to next file.
   ) else (
     mklink /D vimperator %batchdir%vimperator
   )
-
-  :: Link files
-  if exist _vimrc (
-    echo "_vimrc" already exists. Skipping to next file.
-  ) else (
-    mklink /D _vimrc %batchdir%vim\vimrc
-  )
   if exist _vimperatorrc (
     echo "_vimperatorrc" already exists. Skipping to next file.
   ) else (
-    mklink /D _vimperatorrc %batchdir%vimperator\vimperatorrc
+    mklink _vimperatorrc %batchdir%vimperator\vimperatorrc
   )
 
   if %errorLevel% == 0 (
-    echo.
-    echo All files are linked correctly.
-    goto end
+    goto success
+  ) else (
+    goto error
   )
 
-:: ------------------------------------------------------------------------
+rem End program ------------------------------------------------------------
+:error
+  echo.
+  echo Error: Something went wrong.
+  goto end
+
+:success
+  echo.
+  echo Success: All files liked correctly.
+  goto end
+
 :end
   echo.
   pause
