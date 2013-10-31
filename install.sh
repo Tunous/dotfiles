ce206a6dcff7b1f71facf9c5837bf48d54c23a99
Tunous <lukus178@gmail.com>
2013-10-31 17:10:29 +0100

    Add git repository cloning/updating functions

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ msg() {
   printf '%b\n' "$1" >&2
 }
 
+title() {
+  msg "\n---------------------------------------------------"
+  msg "> $1\n"
+}
+
 result() {
   if [ "$1" -eq '0' ]; then
     [ -z "$2" ] ||
@@ -25,6 +30,28 @@ ProgramExists() {
   result "$ret" "" "To continue you first need to install $1." !
 }
 
+UpdateRepo() {
+  title "Trying to update $1..."
+
+  cd "$2" &&
+    git pull origin master
+
+  result "$?" "Successfully updated $1" "Failed to update $1"
+}
+
+CloneRepo() {
+  if [ ! -e "$2/.git" ]; then
+    title "Clonning ${1}..."
+    git clone "$3" "$2"
+    result "$?" "Successfully cloned $1" "Failed to clone $1"
+  else
+    UpdateRepo "$1" "$2"
+  fi
+}
+
 # Check path
 ProgramExists "vim"
 ProgramExists "git"
+
+# Clone/update dotfiles repository
+CloneRepo "$repoName" "$repoDir" 'http://github.com/Tunous/dotfiles.git'
