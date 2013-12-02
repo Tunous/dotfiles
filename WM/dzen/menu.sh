121c30e2e731f884a0d27ec3c1d3d94d4e1ac2da
Tunous <lukus178@gmail.com>
2013-12-02 20:42:22 +0100

    Update menu.sh with more options

--- WM/dzen/menu.sh
+++ WM/dzen/menu.sh
@@ -1,21 +1,28 @@
+#!/bin/bash
+. $HOME/dotfiles/WM/dzen/common.sh
+
 Exec() {
   read prog
 
   case $prog in
-    URxvt)
-      exec urxvt &
-      ;;
-    Shutdown)
-      exec urxvt -name 'URxvtFloating' -geometry '50x2' -e sh -c 'echo "Enter your password to shutdown computer:" && sudo shutdown -h now'
-      ;;
-    Logout)
-      exec urxvt -name 'URxvtFloating' -geometry '50x2' -e sh -c 'echo "Are you sure you want to logout (Y/n)" && read answer && [ $answer = Y ] && (pkill panel; pkill dzen2; bspc quit)'
-      ;;
+    Term)         exec urxvt & ;;
+    Float*Term)   exec urxvt -name 'URxvtFloating' & ;;
+    Web)          exec firefox & ;;
+
+    Run)          dmenu_run -nb "#262626" -nf "#7d7d7d" -sb "#87af00" -sf "#262626" -p "Exec:" -fn "DejaVu Sans" ;;
+    Logout)       terminalPrompt logout ;;
+    Reboot)       terminalPrompt reboot ;;
+    Shutdown)     terminalPrompt shutdown ;;
   esac
 }
 
-(echo "Menu"
-echo "URxvt"
-echo "Shutdown"
-echo "Logout"
-) | dzen2 -m -p -l 5 -tw 1 -w 100 -sa l -h 18 -e 'onstart=uncollapse;leaveslave=collapse,exit;button2=exit;button1=menuprint,exit;button3=exit;button4=scrollup;button5=scrolldown' | Exec
+(echo
+echo Term
+echo Float Term
+echo Web
+echo ──────────────────────────
+echo Run
+echo Logout
+echo Reboot
+echo Shutdown
+) | dzen2 -m -p -l 8 -tw 1 -w 100 -sa l -h 18 -e 'onstart=uncollapse;leaveslave=exit,hide;button2=exit;button1=menuprint,exit;button3=exit;button4=scrollup;button5=scrolldown' | Exec
