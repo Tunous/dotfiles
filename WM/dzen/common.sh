4177277651dcd090753fe84549f68211fbc3a53b
Tunous <lukus178@gmail.com>
2013-12-02 20:38:17 +0100

    Move dzen helper functions to separate script
    For easier accessing from different dzen scripts.

---
+++ WM/dzen/common.sh
@@ -0,0 +1,21 @@
+Align() {
+  case $1 in
+    left)   printf "^p(_LEFT)"    ;;
+    center) printf "^p(_CENTER)"  ;;
+    right)  printf "^p(_RIGHT)"   ;;
+  esac
+
+  [ -n $2 ] && printf "^p($2)"
+}
+
+Separate() {
+  printf "     "
+}
+
+Icon() {
+  printf "^fg(#505050) ^i(%s) ^fg()" "$HOME/dotfiles/WM/dzen/icons/${1}.xbm"
+}
+
+Script() {
+  printf "pkill $1 || $HOME/dotfiles/WM/dzen/$1"
+}
