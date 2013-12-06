45184e431ebd11fa07048abfe04a2590d553f0de
Tunous <lukus178@gmail.com>
2013-12-06 18:10:20 +0100

    Remove terminalPrompt script

--- WM/dzen/menu.sh
+++ WM/dzen/menu.sh
@@ -3,26 +3,25 @@
 
 Exec() {
   read prog
-
   case $prog in
     Term)         exec urxvt & ;;
     Float*Term)   exec urxvt -name 'URxvtFloating' & ;;
     Web)          exec firefox & ;;
 
     Run)          dmenu_run -nb "#262626" -nf "#7d7d7d" -sb "#87af00" -sf "#262626" -p "Exec:" -fn "DejaVu Sans" ;;
-    Logout)       terminalPrompt logout ;;
-    Reboot)       terminalPrompt reboot ;;
-    Shutdown)     terminalPrompt shutdown ;;
+    # Logout)       ;;
+    # Reboot)       ;;
+    # Shutdown)     ;;
   esac
 }
 
 (echo
-echo Term
-echo Float Term
-echo Web
+echo '   Term'
+echo '   Float Term'
+echo '   Web'
 echo ──────────────────────────
-echo Run
-echo Logout
-echo Reboot
-echo Shutdown
-) | dzen2 -m -p -l 8 -tw 1 -w 100 -sa l -h 18 -e 'onstart=uncollapse;leaveslave=exit,hide;button2=exit;button1=menuprint,exit;button3=exit;button4=scrollup;button5=scrolldown' | Exec
+echo '   Run'
+echo 'X Logout'
+echo 'X Reboot'
+echo 'X Shutdown'
+) | dzen2 -m -p -l 8 -tw 1 -w 100 -sa l -e 'onstart=uncollapse;button2=exit;button1=menuprint;button3=exit' | Exec
