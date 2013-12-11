4a340349a2181cdfe401ff8d9836b3afbbd952d5
Tunous <lukus178@gmail.com>
2013-12-11 20:45:16 +0100

    Add reboot and shutdown actions

--- WM/dzen/menu.sh
+++ WM/dzen/menu.sh
@@ -10,8 +10,8 @@ Exec() {
 
     Run)          dmenu_run -nb "#262626" -nf "#7d7d7d" -sb "#87af00" -sf "#262626" -p "Exec:" -fn "DejaVu Sans" ;;
     # Logout)       ;;
-    # Reboot)       ;;
-    # Shutdown)     ;;
+    Reboot)       exec urxvt -geometry 50x1 -name 'URxvtFloating' -e sudo shutdown -r now & ;;
+    Shutdown)     exec urxvt -geometry 50x1 -name 'URxvtFloating' -e sudo shutdown -h now & ;;
   esac
 }
 
@@ -22,6 +22,6 @@ echo '   Web'
 echo ──────────────────────────
 echo '   Run'
 echo 'X Logout'
-echo 'X Reboot'
-echo 'X Shutdown'
+echo '   Reboot'
+echo '   Shutdown'
 ) | dzen2 -m -p -l 8 -tw 1 -w 100 -sa l -e 'onstart=uncollapse;button2=exit;button1=menuprint,exit;button3=exit' | Exec
