b24d388c5a2d3b05fbf49e4f4ff2c5eaeb25d4e4
Tunous <lukus178@gmail.com>
2013-12-14 12:52:35 +0100

    Make wallpaper.sh real wallpaper script

--- Scripts/wallpaper.sh
+++ Scripts/wallpaper.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-files=($1/*)
-echo "${files[RANDOM % ${#files[@]}]}"
+files=($1/*.{png,jpg})
+
+# Make sure that all files are correct
+for i in ${files[@]}; do
+  if [[ -f "$i" ]]; then
+    wanted=("${wanted[@]}" "$i")
+  fi
+done
+
+result="${wanted[RANDOM % ${#wanted[@]}]}"
+
+hsetroot -fill "$result"
