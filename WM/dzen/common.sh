1d1b92f33251c7c0849b5895a97d48e7609a0036
Tunous <lukus178@gmail.com>
2013-12-14 19:47:43 +0100

    Add more colors (from Xresources)

--- WM/dzen/common.sh
+++ WM/dzen/common.sh
@@ -10,14 +10,34 @@ Align() {
 
 Color() {
   case $1 in
-    red)     printf "#af0000" ;;
-    green)   printf "#87af00" ;;
-    yellow)  printf "#af8700" ;;
-    blue)    printf "#0087af" ;;
-    magenta) printf "#6c81c4" ;;
-    cyan)    printf "#2aa198" ;;
+    # Basic
+    [Bb]ackground) printf "#262626" ;;
+    [Ff]oreground) printf "#9e9e9e" ;;
+    [Cc]omment)    printf "#606060" ;;
+    [Ss]election)  printf "#444444" ;;
+    [Ll]ine)       printf "#303030" ;;
 
-    *)       printf "#444444" ;;
+    [Ww]hite)      printf "#ffffff" ;;
+    [Bb]lack)      printf "#000000" ;;
+
+    # Bright
+    [Rr]ed)        printf "#af0000" ;;
+    [Gg]reen)      printf "#87af00" ;;
+    [Yy]ellow)     printf "#af8700" ;;
+    [Bb]lue)       printf "#0087af" ;;
+    [Mm]agenta)    printf "#6c81c4" ;;
+    [Cc]yan)       printf "#2aa198" ;;
+
+    # Dark
+    [Dd][Rr]ed)       printf "#a54242" ;;
+    [Dd][Gg]reen)     printf "#8c9400" ;;
+    [Dd][Yy]ellow)    printf "#d75f00" ;;
+    [Dd][Bb]lue)      printf "#5f819d" ;;
+    [Dd][Mm]agenta)   printf "#85678f" ;;
+    [Dd][Cc]yan)      printf "#5e8d87" ;;
+
+    # Unknown
+    *)          printf "#9e9e9e" ;;
   esac
 }
 
