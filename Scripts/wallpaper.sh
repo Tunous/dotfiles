0ac4294d5b5ac140b425f5042f370531c63b19a1
Tunous <lukus178@gmail.com>
2013-12-14 18:51:01 +0100

    Add option to set wallpaper from single file

--- Scripts/wallpaper.sh
+++ Scripts/wallpaper.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-files=($1/*.{png,jpg})
+if [[ -d $1 ]]; then
+  files=(${1}/*.{png,jpg})
 
-# Make sure that all files are correct
-for i in ${files[@]}; do
-  if [[ -f "$i" ]]; then
-    wanted=("${wanted[@]}" "$i")
+  # Make sure that all files are correct
+  for i in ${files[@]}; do
+    if [[ -f $i ]]; then
+      wanted=("${wanted[@]}" "$i")
+    fi
+  done
+
+  # Select random file
+  result="${wanted[RANDOM % ${#wanted[@]}]}"
+elif [[ -f $1 ]]; then
+  # Get extension
+  extension="${1##*.}"
+
+  # Make sure that it's correct image
+  if [[ $extension =~ png|jpg ]]; then
+    result="$1"
   fi
-done
+fi
 
-result="${wanted[RANDOM % ${#wanted[@]}]}"
-
-hsetroot -fill "$result"
+# Set wallpaper
+if [[ -e $result ]]; then
+  hsetroot -fill "$result"
+else
+  echo "Can't set wallpaper"
+fi
