dae3dd19971d1c6c9123b8337010554274c6d7cb
Tunous <lukus178@gmail.com>
2013-12-14 18:52:19 +0100

    Add colors function

--- WM/dzen/common.sh
+++ WM/dzen/common.sh
@@ -8,14 +8,27 @@ Align() {
   [ -n $2 ] && printf "^p($2)"
 }
 
-Separate() {
-  printf "     "
+Color() {
+  case $1 in
+    red)     printf "#af0000" ;;
+    green)   printf "#87af00" ;;
+    yellow)  printf "#af8700" ;;
+    blue)    printf "#0087af" ;;
+    magenta) printf "#6c81c4" ;;
+    cyan)    printf "#2aa198" ;;
+
+    *)       printf "#444444" ;;
+  esac
 }
 
 Icon() {
-  printf "^fg(#444444) ^i(%s) ^fg()" "$HOME/dotfiles/WM/dzen/icons/${1}.xbm"
+  printf "^fg($(Color "$2")) ^i(%s) ^fg()" "$HOME/dotfiles/WM/dzen/icons/${1}.xbm"
 }
 
 Script() {
   printf "pkill '$1'; $HOME/dotfiles/WM/dzen/$1"
+}
+
+Separate() {
+  printf "     "
 }
