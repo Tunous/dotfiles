81c43fbdfae94732a015b87237a19c0b8aa1582e
Tunous <lukus178@gmail.com>
2013-12-27 19:34:50 +0100

    Minor changes

--- WM/dzen/volume.sh
+++ WM/dzen/volume.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-screen=$(xdpyinfo | grep 'dimensions:' | egrep -o "[0-9]+x[0-9]+ pixels" | egrep -o "[0-9]+x[0-9]+")
-scrx=$(echo $screen | egrep -o "[0-9]+x" | egrep -o "[0-9]+")
-posx=$(($scrx - 200))
-
 stat() {
   if [ "$mut" == "[on]" ]; then
     printf "${vol}%%"
@@ -21,4 +17,4 @@ while :; do
   printf "^fg(#505050)^ca(1,amixer set Master 1+) %s ^ca()" " ▸ "
   echo
   sleep 1
-done | dzen2 -x $posx -y 26 -w 175 -h 20 -ta r -e 'button1=exec:amixer -D pulse set Master toggle;button2=exit;button3=exit'
+done | dzen2 -x -200 -y 26 -w 175 -h 20 -ta r -e 'button1=exec:amixer -D pulse set Master toggle;button2=exit;button3=exit'
