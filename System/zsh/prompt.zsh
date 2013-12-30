6688b5b055386abef84b891db21e70106ef9ed42
Tunous <lukus178@gmail.com>
2013-12-30 14:19:15 +0100

    Remove root specific prompt

--- System/zsh/prompt.zsh
+++ System/zsh/prompt.zsh
@@ -1,16 +1,10 @@
 # Possible states
 VIInsert="%{$fg[white]%} » %{$reset_color%}"
 VINormal="%{$fg_bold[black]%} » %{$reset_color%}"
-VIInsertRoot="%{$fg[red]%} » %{$reset_color%}"
-VINormalRoot="%{$fg_bold[red]%} » %{$reset_color%}"
 
 # Current state
 function zle-line-init zle-keymap-select {
-  if [[ "$(whoami)" = "root" ]]; then
-    VIMode="${${KEYMAP/(vicmd|opp)/$VINormalRoot}/(main|viins)/$VIInsertRoot}"
-  else
-    VIMode="${${KEYMAP/(vicmd|opp)/$VINormal}/(main|viins)/$VIInsert}"
-  fi
+  VIMode="${${KEYMAP/(vicmd|opp)/$VINormal}/(main|viins)/$VIInsert}"
 
   # Actual prompt
   PROMPT="
