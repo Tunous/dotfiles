4db57a02c7f0a990c82f2b38f393bddf20f5d1ba
Tunous <lukus178@gmail.com>
2014-01-02 18:49:40 +0100

    Fix date and pacman scripts

--- WM/dzen/pacman.sh
+++ WM/dzen/pacman.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 pacmanlines=$(pacman -Qu | wc -l)
-updates=$(pacman -Qu)
+updates=$(pacman -Quq)
 lines=$(( $pacmanlines + 2 ))
 
 (
-echo Updates
-echo $updates
-echo
+echo Updates:
+echo "$updates"
+echo -------------------------
 echo Middle click to update
 ) | dzen2 -p 15 -x -230 -y 26 -w 150 -l $lines -sa c -e 'onstart=uncollapse;button1=exit;button2=exec:urxvt -name URxvtFloating,exit;button3=exit'
