5e6b9d243d46a573b02dcbb037362bdb44897c3c
Tunous <lukus178@gmail.com>
2014-01-05 18:56:26 +0100

    Update panel and dzen popups

--- WM/dzen/date.sh
+++ WM/dzen/date.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cal -m | sed -re "s/(^|[ ])($(date +%_d))($|[ ])/\1^fg(#000000)^bg(#9e9e9e)\2^fg()^bg()\3/" | dzen2 -p 15 -x -140 -y 26 -w 140 -l 8 -sa c -e 'onstart=uncollapse;button1=exit;button3=exit'
+cal -m | sed -re "s/(^|[ ])($(date +%_d))($|[ ])/\1^fg(#0087af)\2^fg()\3/" | dzen2 -p 15 -x -147 -y 27 -w 135 -l 7 -sa c -e 'onstart=uncollapse;button1=exit;button3=exit'
--- WM/dzen/pacman.sh
+++ WM/dzen/pacman.sh
@@ -9,4 +9,4 @@ echo Updates:
 echo "$updates"
 echo -------------------------
 echo Middle click to update
-) | dzen2 -p 15 -x -230 -y 26 -w 150 -l $lines -sa c -e 'onstart=uncollapse;button1=exit;button2=exec:urxvt -name URxvtFloating,exit;button3=exit'
+) | dzen2 -p 15 -x -360 -y 27 -w 150 -l $lines -sa c -e 'onstart=uncollapse;button1=exit;button2=exec:urxvt -name URxvtFloating,exit;button3=exit'
