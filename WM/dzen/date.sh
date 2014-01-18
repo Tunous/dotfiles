1b940d7249ec13034c43e92c3b1d4b3941bffdb5
Tunous <lukus178@gmail.com>
2014-01-18 10:41:57 +0100

    Remove dzen configurations

--- WM/dzen/date.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cal -m | sed -re "s/(^|[ ])($(date +%_d))($|[ ])/\1^fg(#0087af)\2^fg()\3/" | dzen2 -p 15 -x -147 -y 27 -w 135 -l 7 -sa c -e 'onstart=uncollapse;button1=exit;button3=exit'
