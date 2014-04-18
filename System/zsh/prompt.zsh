2c8b95ae1f4afe1c92f67bf06df2824725137468
Tunous <lukus178@gmail.com>
2014-04-18 16:55:30 +0200

    [Prompt] Add current git branch info

--- System/zsh/prompt.zsh
+++ System/zsh/prompt.zsh
@@ -1,13 +1,27 @@
+# Current git branch
+setopt prompt_subst
+autoload -Uz vcs_info
+
+zstyle ':vcs_info:*' actionformats '[%b|%a]'
+zstyle ':vcs_info:*' formats       '[%b]'
+zstyle ':vcs_info:*' enable git
+
+vcs_info_wrapper() {
+  vcs_info
+  if [ -n "$vcs_info_msg_0_" ]; then
+    echo "${vcs_info_msg_0_}$del"
+  fi
+}
+
 # Possible states
 VIInsert="%{$fg[white]%} » %{$reset_color%}"
 VINormal="%{$fg_bold[black]%} » %{$reset_color%}"
 
-# Current state
 function zle-line-init zle-keymap-select {
   # Actual prompt
   PROMPT="
 ${${KEYMAP/(vicmd|opp)/$VINormal}/(main|viins)/$VIInsert}"
-  RPROMPT="%{$fg[white]%}%~%{$reset_color%}"
+RPROMPT="%{$fg[white]%}%~ $(vcs_info_wrapper)%{$reset_color%}"
 
   zle reset-prompt
   zle -R
